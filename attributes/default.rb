3618a887be31c1392cfeac9a6c9b8b2ffb9d7ad0
Dan Olaru <dan.olaru@reisystems.com>
2015-03-03 22:21:22 -0500

    fix apache-solr 1.4.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,10 +5,10 @@
 # Copyright 2013, David Radcliffe
 #
 
-default['solr']['version']  = '4.6.1'
-default['solr']['url']      = "https://archive.apache.org/dist/lucene/solr/#{node['solr']['version']}/solr-#{node['solr']['version']}.tgz"
-default['solr']['data_dir'] = '/etc/solr'
-default['solr']['dir']      = '/opt/solr'
+default['solr']['version']  = '1.4.1'
+default['solr']['url']      = "https://archive.apache.org/dist/lucene/solr/#{node['solr']['version']}/apache-solr-#{node['solr']['version']}.tgz"
+default['solr']['data_dir'] = '/etc/solr/conf'
+default['solr']['dir']      = '/usr/share/solr'
 default['solr']['port']     = '8984'
 default['solr']['pid_file'] = '/var/run/solr.pid'
 default['solr']['log_file'] = '/var/log/solr.log'
